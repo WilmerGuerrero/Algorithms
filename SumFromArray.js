26e17c1bcbd1eb5d21fca3ce531544a88b512364
Wilmer Guerrero <wilmermanuel.guerrero25@gmail.com>
2021-06-14 18:55:37 -0400

    Update SumFromArray.js

--- SumFromArray.js
+++ SumFromArray.js
@@ -9,13 +9,15 @@ class SumFromArray{
         for(let i=0;i<array.length;i++){
             for(let j=1;j<array.length; j++){
                 if(array[i]+array[j]==result)
+                {
                     console.log(`The integers within the array that their sum is equal to ${result} are ${array[i]} and ${array[j]}`); 
+                }
                 else
-                    j++;
+                i+2
             }
         }            
     }
 }
 
 const findSumElementsFromArray = new SumFromArray();
-findSumElementsFromArray.findSumElements([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 10);
+findSumElementsFromArray.findSumElements([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 30);
